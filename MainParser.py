aca78db8fa727f95447c3f337eb411fc35338f62
Kishore <skishorekanna@gmail.com>
2020-05-31 12:50:29 +0530

    Fixed the serial number not displayed in order

--- MainParser.py
+++ MainParser.py
@@ -3,6 +3,7 @@ This is the utility for parsing the android logs
 """
 
 import re
+from collections import OrderedDict
 
 class LogParser:
     def __init__(self, filepath, process_id, search_strings):
@@ -17,7 +18,7 @@ class LogParser:
         self.pattern = "\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}  (\d{4})  \d{4} ([A-Z] \w*\s*:\s*) (.*)"
         self.stack_header_pattern = "\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}  (\d{4})  \d{4} (E) (\w*AndroidRuntime\w*): (.*)"
         self.stack_pattern = "\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}  (\d{4})  \d{4} (E) (\w*AndroidRuntime\w*):.*at (.*)"
-        self.output = {}
+        self.output = OrderedDict({})
         self.newline = "\n"
         def read_file(path):
             with open(path,encoding='utf-8') as f:
@@ -81,26 +82,27 @@ class LogParser:
                 self.output[header]["content"]=content
                 self.output[header]["number"]=number
 
+
     def print_fatalexceptions(self):
         """ To print only the fatal exceptions """
         print("\nFATAL EXCEPTION")
         print("===============")
-        for key, value in self.output.items():
-            print("#{0}){1}|{2}".format(value["number"],key,value["count"]))
+        for index,(key, value) in enumerate(self.output.items(),start=1):
+            print("#{0}){1}|{2}".format(index,key,value["count"]))
 
     def print_stacktraces(self):
         print("\nStacktrace")
         print("==========")
-        for key, value in self.output.items():
+        for index, (key, value) in enumerate(self.output.items(),start=1):
             if value["content"]:
-                print("#{0}){1}".format(value["number"],key+self.newline+value["content"]))
+                print("#{0}){1}".format(index,key+self.newline+value["content"]))
 
     def print_errors(self):
         """ To print only the errors"""
         print("\nErrors")
         print("=======")
-        for key, value in self.output.items():
-            print("#{0}){1}|{2}".format(value["number"],key,value["count"]))
+        for index, (key, value) in enumerate(self.output.items(),start=1):
+            print("#{0}){1}|{2}".format(index,key,value["count"]))
 
     def print_matchingstrings(self):
         """ To print only the matching strings"""
